a856033b8bc6529345a77bbe13da6a761bd25b93
DESKTOP-1SFM84C\gomi <gomi@hbut.edu.cn>
2023-12-20 14:42:22 +0800

    修复控件引用

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
 
     const [icsData, setIcsData] = useState("");
 
+    const icsDataRef = useRef(null);
     const startDateRef = useRef(null);
     const timeTableRef = useRef(null);
     const calDataRef = useRef(null);
@@ -17,8 +18,7 @@ export default function Home() {
         let time_table = eval("(" + timeTableRef.current.value + ")");
         let cal_data = eval("(" + calDataRef.current.value + ")");
 
-        let ics_data = eventsToICS(start_date, time_table, cal_data);
-        setIcsData(ics_data);
+        icsDataRef.current.value = eventsToICS(start_date, time_table, cal_data);
 
     }
 
@@ -79,7 +79,7 @@ export default function Home() {
                     Preview</label>
                 <div className="mt-2">
                     <textarea id="ics_data" name="ics_data" rows="3"
-                              value={icsData}
+                              ref={icsDataRef}
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                 </div>
                 <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
